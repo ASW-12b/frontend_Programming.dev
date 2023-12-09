69ccc96721401528c65216061950b36489d78007
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2023-12-09 11:56:06 +0100

    missatge error

--- programming_dev/src/components/Posts.jsx
+++ programming_dev/src/components/Posts.jsx
@@ -1,79 +1,92 @@
-import { useState, useEffect } from 'react'
-import '../styles/posts.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment } from '@fortawesome/free-regular-svg-icons'
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
-import { faStar } from '@fortawesome/free-regular-svg-icons'
-import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
-import { faTrashCan } from '@fortawesome/free-regular-svg-icons'
+import React, { useState, useEffect } from 'react';
+import '../styles/posts.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faArrowUp, faArrowDown, faStar, faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Dropdown } from 'react-bootstrap';
 
 export function Posts() {
-    const [posts, setPosts] = useState([])
-      function getPosts() {
-        return fetch('https://apiprogrammingdev.onrender.com/posts')
-              .then(response => response.json())
-              .then(data => {
-                return data
-              })
-      }
+  const [posts, setPosts] = useState([]);
+  const [selectedOrder, setSelectedOrder] = useState('Nou'); // Default value
+  const [selectedButton, setSelectedButton] = useState('ToT'); // Default value
+  const [messages, setMessages] = useState([]);
 
-      useEffect(() => {
-        const fetchPosts = async () => {
-            const publis = await getPosts()
-            setPosts(publis)
+  function getPosts(order, button) {
+    return fetch(`https://apiprogrammingdev.onrender.com/posts?Tipus_Ordenacio=${order}&Filtre=${button}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.message) {
+          setMessages([data.message]);
+          return [];
+        } else {
+          setMessages([]);
+          return data;
         }
-        fetchPosts()
-      },[])
+      });
+  }
 
-      return (
-        <div className='form-box'>
-            {posts.map(p => {
-                return (
-                    <div key={p.pk} className="col-12 col-lg-6 offset-lg-3 mb-4">
-                            <a style={{color:'black',textDecoration:'none'}} href="#">
-                                <h4><b>{p.fields.title}</b></h4>
-                            </a>
-                        <div className="row px-4">
-                            <div className="col-auto mr-2">
-                                    <a href="" className="link"><FontAwesomeIcon icon={faComment} /></a>
-                            </div>
-                            <div className="col-auto mr-2">
-                                    <p>{p.fields.numComments}</p>
-                            </div>
-                            <div className="col-auto mr-2">
-                                    <a href="" className="link"><FontAwesomeIcon icon={faArrowUp} /></a>
-                            </div>
-                            <div className="col-auto">
-                                <p className="mr-2">
-                                    {p.fields.totalVotes}
-                                </p>
-                            </div>
-                            <div className="col-auto mr-2">
-                                    <a href="" className="link"><FontAwesomeIcon icon={faArrowDown} /></a>
-                            </div>
-                            <div className="col-auto mr-2">
-                                    <a href="" className="link"><FontAwesomeIcon icon={faStar} /></a>
-                            </div>
-                            <div className="dropdown col-auto">
-                                <Dropdown>
-                                    <Dropdown.Toggle variant="light" id="dropdown-basic">
-                                        &#8942;
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item href=""><FontAwesomeIcon icon={faPenToSquare} />Editar</Dropdown.Item>
-                                        <Dropdown.Item href=""><FontAwesomeIcon icon={faTrashCan} />Eliminar</Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
-                            </div>
-                        </div>
-                        <hr className="my-3"></hr>
-                    </div>
-                )
-            })}
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const publis = await getPosts(selectedOrder, selectedButton);
+      setPosts(publis);
+    };
+    fetchPosts();
+  }, [selectedOrder, selectedButton]);
+
+  // Handle order change
+  const handleOrderChange = (eventKey) => {
+    setSelectedOrder(eventKey);
+  };
+
+  // Handle button click
+  const handleButtonClick = (button) => {
+    setSelectedButton(button);
+  };
+
+  return (
+    <div className='form-box'>
+        {/* Display messages */}
+        {messages.map((message, index) => (
+        <div key={index} className="alert alert-danger" role="alert">
+            {message}
         </div>
-        
-      ) 
+        ))}
+
+        {/* Dropdown menu */}
+        <Dropdown onSelect={handleOrderChange} className="mb-3">
+            <Dropdown.Toggle variant="light" id="order-dropdown">
+                {selectedOrder}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item eventKey="Nou">Nou</Dropdown.Item>
+                <Dropdown.Item eventKey="Antic">Antic</Dropdown.Item>
+                <Dropdown.Item eventKey="Mes Votat">Mes Votat</Dropdown.Item>
+                <Dropdown.Item eventKey="Mes Comentaris">Mes Comentaris</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+
+        {/* Buttons */}
+        <div className="btn-group" role="group" aria-label="Basic example">
+            <button className="btn btn-secondary rareButton" onClick={() => handleButtonClick('Subscrit')}>Subscrit</button>
+            <button className="btn btn-secondary rareButton" onClick={() => handleButtonClick('Tot')}>Tot</button>
+        </div>
+
+        {posts.length > 0 ? (
+            posts.map(p => (
+                <div key={p.pk} className="col-12 col-lg-6 offset-lg-3 mb-4">
+                    <a style={{color:'black',textDecoration:'none'}} href={`/posts/${p.pk}`}>
+                        <h4><b>{p.fields.title}</b></h4>
+                    </a>
+                </div>
+            ))
+        ) : (
+            <p>No hi ha cap posts per mostrar</p>
+        )}
+    </div>
+  )
 }
