22ab03dfb8dad23aeabe03b3af5db5c7b69a771b
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2023-12-17 11:40:16 +0100

    afegit retocs estetics

--- programming_dev/src/components/Index.jsx
+++ programming_dev/src/components/Index.jsx
@@ -5,6 +5,7 @@ import { faArrowDown,faArrowUp } from '@fortawesome/free-solid-svg-icons'
 import { /*faTrashCan,faPenToSquare ,*/faStar,faComment} from '@fortawesome/free-regular-svg-icons'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Dropdown } from 'react-bootstrap';
+import {getInfo} from '../controllers/CtrlPosts'
 
 export function Index() {
     const [info, setInfo] = useState([]);
@@ -13,52 +14,26 @@ export function Index() {
     const [selectedButton2, setSelectedButton2] = useState('Publicacions'); // Default value
     const [messages, setMessages] = useState([]);
 
-  function getInfo(order, button, button2) {
-    let apiUrl;
 
-    // Define different API URLs based on the button value
-    if (button2 === 'Publicacions') {
-        apiUrl = 'https://apiprogrammingdev.onrender.com/posts';
-    } else if (button2 === 'Comentaris') {
-        apiUrl = 'https://apiprogrammingdev.onrender.com/comments'; // Adjust the actual API endpoint for comments
-    }
-
-    const url = `${apiUrl}?Tipus_Ordenacio=${order}&Filtre=${button}`;
-    console.log('Fetching data from:', url);
-
-    return fetch(url, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then(response => response.json())
-        .then(data => {
-             console.log('Data fetched:', data);
-            if (data.message) {
-                setMessages([data.message]);
-                return [];
-            } else {
-                setMessages([]);
-            return data;
-            }
-        });
-  }
-
-  useEffect(() => {
+    useEffect(() => {
     const fetchInfo = () => {
         getInfo(selectedOrder, selectedButton, selectedButton2)
-            .then(info => {
-                setInfo(info);
-            })
-        .catch(error => {
-            console.error('Error fetching info:', error);
-            // Handle the error as needed, e.g., set an error message state
-        });
+            .then(result => {
+                if (result.isError) {
+                    console.error('Error fetching info:', result.message);
+                    // Handle the error as needed, e.g., set an error message state
+                    setMessages([result.message]);
+                    setInfo([]); // Clear the data state in case of an error
+                } else {
+                    console.log('PepeInfo:', result.data);
+                    setInfo(result.data);
+                    setMessages([]); // Clear the error message state on success
+                }
+            });
     };
 
     fetchInfo();
-  }, [selectedOrder, selectedButton, selectedButton2]);
+}, [selectedOrder, selectedButton, selectedButton2]);
 
 
 
@@ -76,73 +51,45 @@ export function Index() {
     setSelectedButton2(button);
   };
 
-  return (
-    <div className='form-box'>
-        {/* Dropdown menu */}
-        <Dropdown onSelect={handleOrderChange} className="mb-3">
-            <Dropdown.Toggle variant="light" id="order-dropdown">
-                {selectedOrder}
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                <Dropdown.Item eventKey="Nou">Nou</Dropdown.Item>
-                <Dropdown.Item eventKey="Antic">Antic</Dropdown.Item>
-                <Dropdown.Item eventKey="Mes Comentaris">Mes Comentaris</Dropdown.Item>
-                <Dropdown.Item eventKey="Mes popular">Mes Popular</Dropdown.Item>
-            </Dropdown.Menu>
-        </Dropdown>
+  const renderMessage = (message, index) => (
+    <div key={index} className="alert alert-danger" role="alert">
+      {message}
+    </div>
+  );
 
-        {/* Buttons */}
-        <div className="btn-group rareButton" role="group" aria-label="Basic example">
-            <button className={`btn btn-secondary ${selectedButton === 'Subscrit' ? 'selected' : ''}`} onClick={() => handleButtonClick('Subscrit')}>Subscrit</button>
-            <button className={`btn btn-secondary ${selectedButton === 'Tot' ? 'selected' : ''}`} onClick={() => handleButtonClick('Tot')}>Tot</button>
-        </div>
+  const renderComment = (c, index) => (
+  <div key={index}>
+    <a>{c.content}</a>
+  </div>
+);
 
-        {/* Buttons */}
-        <div className="btn-group rareButton" role="group" aria-label="Basic example">
-            <button className={`btn btn-secondary ${selectedButton2 === 'Publicacions' ? 'selected' : ''}`} onClick={() => handleButtonClick2('Publicacions')}>Publicacions</button>
-            <button className={`btn btn-secondary ${selectedButton2 === 'Comentaris' ? 'selected' : ''}`} onClick={() => handleButtonClick2('Comentaris')}>Comentaris</button>
-        </div>
-
-        {/* Display messages or posts or comentaris*/}
-        {messages.length > 0 ? (
-            messages.map((message, index) => (
-            <div key={index} className="alert alert-danger" role="alert">
-                {message}
-            </div>
-            ))
-        ) : (
-        <>
-            {selectedButton2 === 'Publicacions' ? (
-            // Render posts
-                info.length > 0 ? (
-                    info.map(p => {
-                        return (
-                        <div key={p.pk} className="col-12 col-lg-6 offset-lg-3 mb-4">
-                            <a style={{color:'black',textDecoration:'none'}} href={`/posts/${p.pk}`}>
-                                <h4><b>{p.fields.title}</b></h4>
-                            </a>
-                            <div className="row px-4">
-                                <div className="col-auto mr-2">
-                                        <a href="" className="link"><FontAwesomeIcon icon={faComment} /></a>
-                                </div>
-                                <div className="col-auto mr-2">
-                                        <p>{p.fields.numComments}</p>
-                                </div>
-                                <div className="col-auto mr-2">
-                                        <a href="" className="link"><FontAwesomeIcon icon={faArrowUp} /></a>
-                                </div>
-                                <div className="col-auto">
-                                    <p className="mr-2">
-                                        {p.fields.totalVotes}
-                                    </p>
-                                </div>
-                                <div className="col-auto mr-2">
-                                        <a href="" className="link"><FontAwesomeIcon icon={faArrowDown} /></a>
-                                </div>
-                                <div className="col-auto mr-2">
-                                        <a href="" className="link"><FontAwesomeIcon icon={faStar} /></a>
-                                </div>
-                                {/*
+   const renderPost = (p, index) => (
+       <div key={index} className="col-12 col-lg-6 offset-lg-3 mb-4 filtre">
+               <a style={{color: 'black', textDecoration: 'none'}} href={`/posts/${p.pk}`}>
+                   <h4><b>{p.fields && p.fields.title}</b></h4>
+               </a>
+               <div className="row px-4">
+                   <div className="col-auto mr-2">
+                       <a href="" className="link"><FontAwesomeIcon icon={faComment}/></a>
+                   </div>
+                   <div className="col-auto mr-2">
+                       <p>{p.fields && p.fields.numComments}</p>
+                   </div>
+                   <div className="col-auto mr-2">
+                       <a href="" className="link"><FontAwesomeIcon icon={faArrowUp}/></a>
+                   </div>
+                   <div className="col-auto">
+                       <p className="mr-2">
+                           {p.fields && p.fields.totalVotes}
+                       </p>
+                   </div>
+                   <div className="col-auto mr-2">
+                       <a href="" className="link"><FontAwesomeIcon icon={faArrowDown}/></a>
+                   </div>
+                   <div className="col-auto mr-2">
+                       <a href="" className="link"><FontAwesomeIcon icon={faStar}/></a>
+                   </div>
+                   {/*
                                 <div className="dropdown col-auto">
                                     <Dropdown>
                                         <Dropdown.Toggle variant="light" id="dropdown-basic">
@@ -155,27 +102,57 @@ export function Index() {
                                     </Dropdown>
                                 </div>
                     */}
-                            </div>
-                            <hr className="my-3"></hr>
-                        </div>
-                    )})
-                ) : (
-                    <p>No hi ha cap posts per mostrar</p>
-                )
+               </div>
+               <hr className="my-3"></hr>
+       </div>
+   );
+
+    return (
+        <div className="custom-margin">
+            <div className="container">
+                <div className="filtre">
+                    <Dropdown onSelect={handleOrderChange}>
+                        <Dropdown.Toggle variant="light" id="order-dropdown">
+                            {selectedOrder}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item eventKey="Nou">Nou</Dropdown.Item>
+                            <Dropdown.Item eventKey="Antic">Antic</Dropdown.Item>
+                            <Dropdown.Item eventKey="Mes Comentaris">Mes Comentaris</Dropdown.Item>
+                            <Dropdown.Item eventKey="Mes popular">Mes Popular</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+                <div className="filtre btn-group rareButton" role="group">
+                    <button className={`btn btn-secondary ${selectedButton === 'Subscrit' ? 'selected' : ''}`} onClick={() => handleButtonClick('Subscrit')}>Subscrit</button>
+                    <button className={`btn btn-secondary ${selectedButton === 'Tot' ? 'selected' : ''}`} onClick={() => handleButtonClick('Tot')}>Tot</button>
+                </div>
+                <div className="filtre btn-group rareButton" role="group">
+                    <button className={`btn btn-secondary ${selectedButton2 === 'Publicacions' ? 'selected' : ''}`} onClick={() => handleButtonClick2('Publicacions')}>Publicacions</button>
+                    <button className={`btn btn-secondary ${selectedButton2 === 'Comentaris' ? 'selected' : ''}`} onClick={() => handleButtonClick2('Comentaris')}>Comentaris</button>
+                </div>
+            </div>
+
+            {/* Display messages or posts or comments */}
+            {messages.length > 0 ? (
+                messages.map((message, index) => renderMessage(message, index))
             ) : (
-                info.length > 0 ? (
-                    info.map(c => {
-                        return (
-                            <div key={c.id}>
-                                <a>{c.content}</a>
-                            </div>
+                <>
+                    {selectedButton2 === 'Publicacions' ? (
+                        info.length > 0 ? (
+                            info.map((p, index) => renderPost(p, index))
+                        ) : (
+                            <p>No hi ha cap posts per mostrar</p>
                         )
-                    })
-                ) : (
-                    <p>No hi cap comentari per mostrar</p>
-                ))}
-        </>
-        )}
-    </div>
-  )
+                    ) : (
+                        info.length > 0 ? (
+                            info.map((c, index) => renderComment(c, index))
+                        ) : (
+                            <p>No hi cap comentari per mostrar</p>
+                        )
+                    )}
+                </>
+            )}
+        </div>
+    )
 }
---
+++ programming_dev/src/controllers/CtrlPosts.js
@@ -0,0 +1,32 @@
+export async function getInfo(order, button, button2) {
+    let apiUrl;
+
+    // Define different API URLs based on the button value
+    if (button2 === 'Publicacions') {
+        apiUrl = 'https://apiprogrammingdev.onrender.com/posts';
+    } else if (button2 === 'Comentaris') {
+        apiUrl = 'https://apiprogrammingdev.onrender.com/comments';
+    }
+
+    const url = `${apiUrl}?Tipus_Ordenacio=${order}&Filtre=${button}`;
+    console.log('Fetching data from:', url);
+
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Data fetched:', data);
+            if (data.message) {
+                return { isError: true, message: data.message };
+            }
+            return { isError: false, data: data };
+        })
+        .catch(error => {
+            console.error('Error fetching info:', error);
+            return { isError: true, message: 'An error occurred while fetching data.' };
+        });
+}
